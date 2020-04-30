33f96815f44400ebf7191dede96674dbd8f365b7
juancki <jcgp97@gmail.com>
2020-04-30 22:57:29 +0000

    Is able to introduce the location members into redis automatically

---
+++ go.mod
@@ -0,0 +1,11 @@
+module github.com/juancki/wsholder
+
+go 1.14
+
+require (
+	github.com/go-redis/redis/v7 v7.2.0 // indirect
+	github.com/golang/protobuf v1.4.0
+	google.golang.org/genproto v0.0.0-20200430143042-b979b6f78d84 // indirect
+	google.golang.org/grpc v1.29.1
+	google.golang.org/protobuf v1.21.0
+)
--- main.go
+++ main.go
@@ -2,19 +2,23 @@
 package main
 
 import (
-    "bufio"
-    "encoding/binary"
-    "flag"
-    "fmt"
-    "log"
-    "net"
-    "strconv"
-    "strings"
-    "time"
-    cPool "github.com/juancki/wsholder/connectionPool"
-    "github.com/juancki/wsholder/pb"
-    "github.com/golang/protobuf/proto"
-    "google.golang.org/grpc"
+	"bufio"
+	"encoding/base64"
+	"encoding/binary"
+	"flag"
+	"fmt"
+	"log"
+	"net"
+	"strconv"
+	"strings"
+	"sync"
+	"time"
+
+	"github.com/go-redis/redis/v7"
+	"github.com/golang/protobuf/proto"
+	cPool "github.com/juancki/wsholder/connectionPool"
+	"github.com/juancki/wsholder/pb"
+	"google.golang.org/grpc"
 )
 // Add connection to REDIS and save when a connection gets into front.
 
@@ -26,6 +30,7 @@ import (
 
 
 var pool cPool.ConnectionPool
+const WORLD = "world"
 
 // server is used to implement helloworld.GreeterServer.
 type server struct {
@@ -102,36 +107,107 @@ func connId(conn net.Conn) cPool.Uuid{
     }
 }
 
+func coorFromBase64(str string) (float64,float64){
+    // log.Println("entering coorFromBase64")
+    splits := strings.SplitN(str,":",3)
+    b64 := splits[0]
+    // log.Println("input string: ",str)
+    // log.Println("Section: ",b64)
+    coorbts , _ := base64.StdEncoding.DecodeString(b64)
+    coorstring := string(coorbts)
+    // log.Println("Decoded: ",coorstring)
+
+    ind := strings.Split(coorstring,":")
+    if len(ind) < 2{
+        return -1,-1
+    }
+    long,_ := strconv.ParseFloat(ind[0],64)
+    lat,_ := strconv.ParseFloat(ind[1],64)
+    return long,lat
+}
+
+
+
+func addToPoolAndUpdateRedis(cn net.Conn) error {
+    ID := connId(cn)
+    writer := bufio.NewWriter(cn)
+    writer.WriteString("Ack\n")
+    writer.Flush()
+    // ADD to POOL 
+    pool.Add(cPool.Uuid(ID), cn)
+    log.Println("New connection id:",ID)
+    bts := make([]byte,100) // Decide token size
+    read, err := cn.Read(bts)
+    if err != nil{
+        return err
+    }
+    token := string(bts[1:read-1])
+    // UPDATE REDIS
+    log.Println(token)
+    log.Println("$2a$04$1B2TiNo0fhO1j0pXa/FPYeGOxOm9CRfkVN3pIb5Vaknwfl936/CpC")
+    appendError := rclient.redis.Append(token,cPool.Uuid2base64(ID)).Err()
+    rclient.Get("$2a$04$1B2TiNo0fhO1j0pXa/FPYeGOxOm9CRfkVN3pIb5Vaknwfl936/CpC")
+    value, err := rclient.Get(token)
+    if appendError != nil {
+        log.Println(appendError)
+        return appendError
+    }
+    if err != nil {
+        log.Println(err)
+        return err
+    }
+    // value from the key,value store
+    log.Println("The value is: ", value)
+    long,lat := coorFromBase64(value)
+    geoloc := &redis.GeoLocation{}
+    geoloc.Latitude = lat
+    geoloc.Longitude= long
+    geoloc.Name = cPool.Uuid2base64(ID)
+    rgeoclient.Lock()
+    geoAdd := rgeoclient.redis.GeoAdd(WORLD,geoloc)
+    rgeoclient.Unlock()
+    if geoAdd.Err() != nil{
+        log.Println(geoAdd.Err())
+        return geoAdd.Err()
+    }
+    log.Println("Geoadd succesful")
+    return nil
+}
+
+func manageFrontError(c net.Conn){
+    c.SetWriteDeadline(time.Now().Add(time.Millisecond*2))
+    c.Write([]byte("Error in connection"))
+    c.Close()
+    pool.Remove(connId(c))
+}
 
 func rutineFront(addr string){
     // addr := "127.0.0.1:8080"
-    log.Print("Starting ws front: ",addr)
+    log.Print("Starting TCP ws front: ",addr)
     socket, err := net.Listen("tcp", addr)
     if err != nil {
         log.Print("Could not start tcp.")
     }
+    log.Println("TCP ws back server started")
 
     // accept connection
     for true {
         connection, _ := socket.Accept()
-        ID := connId(connection)
-        writer := bufio.NewWriter(connection)
-        writer.WriteString("Ack\n")
-        writer.Flush()
-        // add connection to pool
-        pool.Add(cPool.Uuid(ID), connection)
-        // TODO add connection to REDIS
-        log.Print("New connection id:",ID)
+        err = addToPoolAndUpdateRedis(connection)
+        if err != nil{
+            manageFrontError(connection)
+        }
     }
 }
 
 func rutineBack(addr string){
     // addr := "127.0.0.1:8090"
-    log.Print("Starting ws back: ",addr)
+    log.Print("Starting gRPC ws back: ",addr)
     lis, err := net.Listen("tcp",addr)
     if err != nil {
         log.Fatalf("failed to listen: %v", err)
     }
+    log.Println("gRPC ws back server started")
     s := grpc.NewServer()
     pb.RegisterWsBackServer(s, &server{})
     if err := s.Serve(lis); err != nil {
@@ -139,16 +215,70 @@ func rutineBack(addr string){
     }
 }
 
-func setupRedis(user string, password string, addr string){
-    // TODO implement
-    log.Print("Redis not configured")
+type Redis struct{
+    redis *redis.Client
+    sync.Mutex
 }
+
+
+// type Postgre struct{
+//     pg sql.DB
+//     sync.Mutex
+// }
+
+
+func NewRedis(connURL string) *Redis{
+    // redis://password@netloc:port/dbnum
+    // redis does not have a username
+    key := "postgresql://"
+    if strings.HasPrefix(connURL,key){
+        connURL = connURL[len(key):]
+    }
+    dbnum,_ := strconv.Atoi(strings.Split(connURL,"/")[1])
+    connURL = strings.Split(connURL,"/")[0]
+
+    pass := strings.Split(connURL,"@")[0]
+    log.Print("`",pass,"`\t",len(pass))
+    addr := strings.Split(connURL,"@")[1]
+    client := redis.NewClient(&redis.Options{
+            Addr:     addr,
+            Password: "", // no password set
+            DB:       dbnum,  // use default DB
+    })
+
+    pong, err := client.Ping().Result()
+    if err != nil{
+        // Exponential backoff
+        log.Print("Un able to connect to REDIS `", dbnum,"` at: `", addr, "` with password: `",pass,"`.")
+        log.Print(err)
+        return nil
+    }
+    log.Print("I said PING, Redis said: ",pong)
+    return &Redis{redis: client}
+
+}
+
+func (*Redis) Get(key string) (string,error){
+    rclient.Lock()
+    val, err := rclient.redis.Get(key).Result()
+    rclient.Unlock()
+    if err != nil {
+        log.Println("Not succesful: ", key, " ", err)
+        return "",err
+    }
+    log.Println("Succesful: ", key, " ", val)
+    return val, nil
+}
+
+var rclient *Redis
+var rgeoclient *Redis
 
 func main() {
     // flag for configuration via command line
     frontport := flag.String("front-port", "localhost:8080", "port to connect (clients)")
     backport := flag.String("back-port", "localhost:8090", "port to connect (server)")
-    redis:= flag.String("redis", ":@localhost:6379", "user:password@IPAddr:port")
+    redis:= flag.String("redis", "@localhost:6379/0", "format password@IPAddr:port")
+    redisGeo := flag.String("redisGeo", "@localhost:6379/1", "format password@IPAddr:port")
     flag.Parse()
     fport := *frontport
     bport := *backport
@@ -159,12 +289,18 @@ func main() {
         bport = ("localhost:" + *backport)
     }
     // Set up redis
-    redisconf := strings.Split(*redis,"@")
-    redisid := strings.Split(redisconf[0],":")
-    redisAddr := redisconf[1]
+    rclient = nil
+    for rclient == nil{
+        rclient = NewRedis(*redis)
+        time.Sleep(time.Second*1)
+    }
+    rgeoclient = nil
+    for rgeoclient == nil{
+        rgeoclient  = NewRedis(*redisGeo)
+        time.Sleep(time.Second*1)
+    }
     // Starting server
     fmt.Println("Starting wsholder...") // ,*frontport,"for front, ",*backport," for back")
-    setupRedis(redisid[0],redisid[1],redisAddr)
     fmt.Println("--------------------------------------------------------------- ")
     pool = *cPool.NewConnectionPool()
     go rutineFront(fport)
@@ -175,8 +311,8 @@ func main() {
 
     for true {
         // TODO: Logging stay alive, remove before prod
-        time.Sleep(5 * time.Second)
         size := pool.Size()
         log.Print("Size: ",size)
+        time.Sleep(10 * time.Second)
     }
 }
